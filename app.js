f169a02dac6dbdae47d962187bfec1c10368c332
Mar Aguayo Florit <154342626+magufl@users.noreply.github.com>
2024-04-10 19:30:24 +0000

    terminado

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-// Contenido de app.js 
-// Declaramos una función con el nombre exacto "formEuroToDollar"
-const fromEuroToDollar = function(valueInEuro) {
-    // Convertimos el valor a dólares
-    let valueInDollar = valueInEuro * 1.07;
-    // Retornamos el valor en dólares
-    return valueInDollar;
+// One euro is:
+
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british poun
+
 }
 
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    let valueInDollar = valueInEuro * oneEuroIs.USD ;
+    return Math.floor(valueInDollar*100)/100;
+};
+    
 // Esta es mi función que pasa de dollars a yen
 const fromDollarToYen = function(valueInDollar) {
-    let valueInYen = ((valueInDollar / 1.07) * 156.5);
-    return valueInYen;
-}
+    let dollarToEuro = valueInDollar / oneEuroIs.USD;
+    let euroToYen = dollarToEuro * oneEuroIs.JPY;
+    return Math.floor(euroToYen * 100) / 100;
+};
 
 // Esta es mi función que pasa de yen a pound
 const fromYenToPound = function(valueInYen) {
-    let valueInPound = ((valueInYen / 156.5) * 0.87);
-    return valueInPound;
-}
+    let yenToEuro = valueInYen / oneEuroIs.JPY;
+    let euroToPound = yenToEuro * oneEuroIs.GBP;
+    return Math.floor(euroToPound * 100) / 100;
+};
 
-// Esta es mi función que suma dos números
-const sum = (a,b) => {
-    return a + b
-}
 
-// Solo un registro en consola para nosotros
-console.log(sum(7,3))
 
 // Tenemos que incluir la función en el exports para que sea exportada a otros archivos como test.js
-module.exports = { sum, fromEuroToDollar }
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound , oneEuroIs }
